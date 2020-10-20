341cadec542ae0a7d65ca6fe254c8e8902777d60
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-10-20 17:07:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicons/android-chrome-192x192.png" type="image/png" sizes="192x192">
+</head>
+
+<body>
+    <!--NavBar-->
+    <?php include "asset/navbar.html"?>
+</body>
+
+</html>
