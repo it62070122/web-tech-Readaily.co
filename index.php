82084273b2b6f38d33011a47a67c7414bd6a9bad
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-10-22 22:53:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,8 +7,35 @@
 </head>
 
 <body>
+    <!-- Head -->
+    <?php include "asset/head.html"?>
     <!--NavBar-->
     <?php include "asset/navbar.html"?>
-</body>
 
+    <!-- Content -->
+        <!-- So hot right now -->
+        <?php include "asset/index/so-hot-right-now.html"?>
+        <!-- Pre order -->
+        <?php include "asset/index/pre-order.html"?>
+        <!-- New Arrivals - Fiction -->
+        <?php include "asset/index/new-arrivals-fiction.html"?>
+        <!-- วรรณกรรมญี่ปุ่น -->
+        <?php include "asset/index/japanese-literature.html"?>
+        <!-- วรรณกรรมแปล -->
+        <?php include "asset/index/fiction-novel.html"?>
+        <!-- New Arrivals - Thai Fiction -->
+        <?php include "asset/index/new-arrivals-thai-fiction.html"?>
+        <!-- Non-Fiction: Bestsellers -->
+        <?php include "asset/index/non-fiction-bestsellers.html"?>
+        <!-- New Arrivals - Non-Fiction -->
+        <?php include "asset/index/new-arrivals-non-fiction.html"?>
+    <!-- End content -->
+
+    <!-- Category box -->
+    <?php include "asset/categorybox.html"?>
+    <!-- Top button -->
+    <?php include "asset/backtotop.html"?>
+    <!-- Footer -->
+    <?php include "asset/footer.html"?>
+</body>
 </html>
