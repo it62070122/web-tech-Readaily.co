227369fc09452455deba72bb7288afaa9c3c5fbd
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-11-01 20:57:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Readery ร้านหนังสือออนไลน์ รี้ดเดอรี่</title>
     <link rel="icon" href="images/favicons/android-chrome-192x192.png" type="image/png" sizes="192x192">
 </head>
 
