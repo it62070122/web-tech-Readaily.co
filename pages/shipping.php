e6c76fae0064417127cdb44b38c17a9338a7b8b4
YANADASANG <neonhardpunch@gmail.com>
2020-11-09 23:30:49 +0700

    Create shipping.php

---
+++ pages/shipping.php
@@ -0,0 +1,47 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>วิธีจัดส่ง</title>
+    <link rel="icon" href="../images/favicons/android-chrome-192x192.png" type="image/png" sizes="192x192">
+</head>
+
+<body>
+
+<?php 
+    // Head 
+    include "../asset/head.html";
+    // NavBar
+    include "../asset/navbar.html";
+?>
+        <div class="row padding-content d-flex justify-content-center">
+            <div class="container">
+                    <h2>วิธีจัดส่ง</h2>
+                    <h5>คิดค่าจัดส่งอย่างไร?</h5>
+                    <p>
+                       ออเดอร์ต่ำกว่า 1,000 บาท<br>
+                       ค่าบริการจัดส่ง กรุงเทพฯ 60 บาท<br>
+                       ค่าบริการจัดส่ง จังหวัดอื่น 80 บาท<br>
+                    </p>
+                    <p>
+                        ส่งฟรีเมื่อซื้อครบ 1,000 บาท<br>
+                        (ยกเว้นบางโปรโมชั่นอาจจะมีค่าจัดส่งกำหนดไว้)
+                    </p>
+                    <h5>จัดส่งด้วยวิธีใด?</h5>
+                    <p>จัดส่งโดยบริษัทขนส่งเอกชน โดยจะมีเจ้าหน้าที่ขนส่งโทรศัพท์ติดต่อลูกค้าเพื่อแจ้งล่วงหน้าในวันที่ทำการจัดส่ง</p>
+                    <h5>สอบถามข้อมูลเพิ่มเติม</h5>
+                    <p>หากมีข้อสงสัย หรือต้องการสอบถามข้อมูลเพิ่มเติม กรุณาติดต่อมาที่ hello@readery.co หรือ โทร. 02-726-7340 (จันทร์-ศุกร์ เวลา 9:00-17:00)</p>
+            </div>
+            
+        </div>
+
+<?php 
+    // Top button
+    include "../asset/backtotop.html";
+    // Footer
+    include "../asset/footer.html";
+?>
+
+</body>
+</html>
