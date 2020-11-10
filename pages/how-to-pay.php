abdeecba490ccda2b02d4734a7163674ddc70ebb
Ketsaraporn Dhetboon <56309662+IVizzReview@users.noreply.github.com>
2020-11-10 10:54:17 +0700

    Create how-to-pay

---
+++ pages/how-to-pay.php
@@ -0,0 +1,69 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ช่องทางการชำระเงิน</title>
+    <link rel="icon" href="../images/favicons/android-chrome-192x192.png" type="image/png" sizes="192x192">
+</head>
+    <style>
+        a.home:link {text-decoration:none;}
+        a.home:visited {text-decoration:none;}
+        a.home:hover {text-decoration:underline;}
+    </style>
+<body>
+
+<?php 
+    // Head 
+    include "../asset/head.html";
+    // NavBar
+    include "../asset/navbar.html";
+?>
+
+<div class="row no-gutters" style="padding: 5vh 5.2vw 5vh 5.2vw; background-color: #f3f3f3;">
+    <div class="container" style="width: 750px; margin: auto;">
+
+        <div class="breadcrumb" style="background-color: #f3f3f3;">
+            <a class="home" href="index.php" title="ไปยังหน้าหลัก" style="color: grey;">Home</a>
+            <span style="color: grey;">&nbsp; » &nbsp;</span>
+            <strong>ช่องทางการชำระเงิน</strong>
+        </div>
+
+        <div class="form-group" style="background-color: white;">
+
+            <div class="form-title">
+                <br>
+            <h1 class="text-center font-weight-bold">ช่องทางการชำระเงิน</h1>
+                <br>
+            <div>
+
+            <div class="col align-self-center" style="padding: 20px 160px">
+                <div class="form-wrapper">
+                    <h4>1. ชำระด้วยวิธีโอนเงิน</h4>
+                    <p>กรุณาเลือกโอนเงินเข้าบัญชีธนาคารด้านล่าง แล้วแจ้งการโอนเงินมายัง hello@readery.co หรือที่หมายเลขโทรศัพท์ 02-726-7340 หรือแจ้งการโอนเงินผ่าน <a href="#แบบฟอร์มแจ้งโอนเงิน">แบบฟอร์มแจ้งโอนเงิน</a></p>
+                    <p><strong>ชื่อบัญชี บจก. รี้ดเดอรี่</strong><br>
+                    ธนาคารกสิกรไทย สาขาพาราไดซ์พาร์ค<br>
+                    เลขที่บัญชี 047-8-84642-8</p>
+                <br>
+                <h4>2. ชำระด้วยบัตรเครดิต ผ่าน PayPal</h4>
+                    <p>หากเลือกวิธีชำระเงินด้วยบัตรเครดิต ผ่าน PayPal เมื่อทำการสั่งซื้อเรียบร้อยแล้ว ระบบจะส่งต่อไปยังหน้าชำระเงินด้วยบัตรเครดิตผ่าน PayPal สามารถเลือกชำระด้วยบัตรเครดิต Visa และ MasterCard เมื่อดำเนินรายการต่อจนเสร็จเรียบร้อยแล้ว จะมีอีเมลแจ้งการชำระเงินจาก PayPal และอีเมลแจ้งรายละเอียดการสั่งซื้อจากรี้ดเดอรี่</p>
+                <br>
+                <h4>สอบถามข้อมูลเพิ่มเติม</h4>
+                    <p>หากมีข้อสงสัย หรือต้องการสอบถามข้อมูลเพิ่มเติม กรุณาติดต่อมาที่ hello@readery.co หรือ โทร. 02-726-7340 (จันทร์-ศุกร์ เวลา 9:00-17:00)</p>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+
+    </div>
+</div>
+    
+<?php 
+    // Top button
+    include "../asset/backtotop.html";
+    // Footer
+    include "../asset/footer.html";
+?>
+</body>
+</html>
