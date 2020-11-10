c3f4a9171902f40a6a20aa7218d524c276909720
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-11-11 01:07:55 +0700

    Update how-to-pay.php

--- pages/how-to-pay.php
+++ pages/how-to-pay.php
@@ -20,11 +20,10 @@
     include "../asset/navbar.html";
 ?>
 
-<div class="row no-gutters" style="padding: 5vh 5.2vw 5vh 5.2vw; background-color: #f3f3f3;">
-    <div class="container" style="width: 750px; margin: auto;">
-
-        <div class="breadcrumb" style="background-color: #f3f3f3;">
-            <a class="home" href="index.php" title="ไปยังหน้าหลัก" style="color: grey;">Home</a>
+<div class="row no-gutters" style="padding: 4.5vh 5.3vw; background-color: #f3f3f3;">
+    <div class="container " style="width: 750px; margin: auto;">
+        <div class="breadcrumb p-0" style="background-color: #f3f3f3;">
+            <a class="home" href="../index" title="ไปยังหน้าหลัก" style="color: grey;">Home</a>
             <span style="color: grey;">&nbsp; » &nbsp;</span>
             <strong>ช่องทางการชำระเงิน</strong>
         </div>
@@ -33,24 +32,28 @@
 
             <div class="form-title">
                 <br>
-            <h1 class="text-center font-weight-bold">ช่องทางการชำระเงิน</h1>
+            <h1 class="text-center font-weight-bold title-pay">ช่องทางการชำระเงิน</h1>
                 <br>
             <div>
 
-            <div class="col align-self-center" style="padding: 20px 160px">
+            <div class="col align-self-center" style="padding: 3vh 7.5vw">
                 <div class="form-wrapper">
-                    <h4>1. ชำระด้วยวิธีโอนเงิน</h4>
-                    <p>กรุณาเลือกโอนเงินเข้าบัญชีธนาคารด้านล่าง แล้วแจ้งการโอนเงินมายัง hello@readery.co หรือที่หมายเลขโทรศัพท์ 02-726-7340 หรือแจ้งการโอนเงินผ่าน <a href="#แบบฟอร์มแจ้งโอนเงิน">แบบฟอร์มแจ้งโอนเงิน</a></p>
+                    <p class="subtitle-pay">1. ชำระด้วยวิธีโอนเงิน</p>
+                    <div class="pay">
+                        <p>กรุณาเลือกโอนเงินเข้าบัญชีธนาคารด้านล่าง แล้วแจ้งการโอนเงินมายัง hello@readery.co หรือที่หมายเลขโทรศัพท์ 02-726-7340 หรือแจ้งการโอนเงินผ่าน <a href="payment.php">แบบฟอร์มแจ้งโอนเงิน</a></p>
                     <p><strong>ชื่อบัญชี บจก. รี้ดเดอรี่</strong><br>
                     ธนาคารกสิกรไทย สาขาพาราไดซ์พาร์ค<br>
                     เลขที่บัญชี 047-8-84642-8</p>
-                <br>
-                <h4>2. ชำระด้วยบัตรเครดิต ผ่าน PayPal</h4>
-                    <p>หากเลือกวิธีชำระเงินด้วยบัตรเครดิต ผ่าน PayPal เมื่อทำการสั่งซื้อเรียบร้อยแล้ว ระบบจะส่งต่อไปยังหน้าชำระเงินด้วยบัตรเครดิตผ่าน PayPal สามารถเลือกชำระด้วยบัตรเครดิต Visa และ MasterCard เมื่อดำเนินรายการต่อจนเสร็จเรียบร้อยแล้ว จะมีอีเมลแจ้งการชำระเงินจาก PayPal และอีเมลแจ้งรายละเอียดการสั่งซื้อจากรี้ดเดอรี่</p>
-                <br>
-                <h4>สอบถามข้อมูลเพิ่มเติม</h4>
-                    <p>หากมีข้อสงสัย หรือต้องการสอบถามข้อมูลเพิ่มเติม กรุณาติดต่อมาที่ hello@readery.co หรือ โทร. 02-726-7340 (จันทร์-ศุกร์ เวลา 9:00-17:00)</p>
+                    </div>
+                    
+                <br><br>
+                <p class="subtitle-pay">2. ชำระด้วยบัตรเครดิต ผ่าน PayPal</p>
+                    <p  class="pay">หากเลือกวิธีชำระเงินด้วยบัตรเครดิต ผ่าน PayPal เมื่อทำการสั่งซื้อเรียบร้อยแล้ว ระบบจะส่งต่อไปยังหน้าชำระเงินด้วยบัตรเครดิตผ่าน PayPal สามารถเลือกชำระด้วยบัตรเครดิต Visa และ MasterCard เมื่อดำเนินรายการต่อจนเสร็จเรียบร้อยแล้ว จะมีอีเมลแจ้งการชำระเงินจาก PayPal และอีเมลแจ้งรายละเอียดการสั่งซื้อจากรี้ดเดอรี่</p>
+                <br><br>
+                <p class="subtitle-pay">สอบถามข้อมูลเพิ่มเติม</p>
+                    <p  class="pay">หากมีข้อสงสัย หรือต้องการสอบถามข้อมูลเพิ่มเติม กรุณาติดต่อมาที่ hello@readery.co หรือ โทร. 02-726-7340 (จันทร์-ศุกร์ เวลา 9:00-17:00)</p>
                 </div>
+                <br>
             </div>
         </div>
         </div>
