b0f06c1241c044e784ecf9b89e2b82f61d50e932
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-11-11 20:34:50 +0700

    Update new-arrivals-non-fiction.php

--- asset/index/new-arrivals-non-fiction.php
+++ asset/index/new-arrivals-non-fiction.php
@@ -7,7 +7,38 @@
 
 <div class="row no-gutters padding-content d-flex justify-content-center">
     <div class="container p-0">
-        <h2 id="title">New Arrivals - Non-Fiction</h2>
+    <h2 id="title">New Arrivals - Non-Fiction</h2>
+        <div class="row align-self-start mt-4">
+            <?php 
+            for ($i=0; $i<count($result); $i++){
+                echo "<div class=\"col-6 col-md\" id=\"books\">";
+                echo "<div class=\"card m-1\" style=\"border: none; text-align: center; padding: 2vh;\">";
+                echo "<img class=\"card-img-top img-fluid ml-auto mr-auto mb-3\" src=\"images/cover/new-arrivals-non-fiction/".$result[$i]->img."\">";
+
+                if($result[$i]->covertag == "new release"){
+                    echo "<div class=\"new-release\"></div>";
+                }
+                else if($result[$i]->covertag == "Pre-Order"){
+                    echo "<div class=\"pre-order\"></div>";
+                }
+
+                echo "<a class=\"thainame mb-1 text-decoration-none\" href=\"\" id=\"book-title\">";
+                echo $result[$i]->thainame;
+                echo "</a>";
+                echo "<a class=\"artist mb-1 text-decoration-none\" href=\"\" id=\"book-author\">";
+                echo $result[$i]->artist;
+                echo "</a>";
+                echo "<p>";
+                echo "<span class=\"price-grey\">";
+                echo "฿".number_format($result[$i]->cost, 2, '.', '');
+                echo "</span>";
+                echo "<span class=\"price-red\">";
+                echo " ฿".number_format(($result[$i]->cost*(100 - $result[$i]->discount))/100, 2, '.', '');
+                echo "</span></p></div></div>";
+            }
+            ?>
+        </div>
     </div>
+    
 </div>
 <hr style="border-top: 1px solid#f0f0f0; margin: 0;">
