a614b8723df78902d9f9d3d5fa0e6d6746273d90
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2020-11-13 21:50:25 +0700

    Fix pages file

--- pages/9786162985102.php
+++ pages/9786162985102.php
@@ -10,11 +10,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>
-            <?php if($result[2]->thainame != null){
-        echo $result[2]->thainame.": ".$result[2]->artist.": ".$result[2]->key;
+            <?php if($result[0]->thainame != null){
+        echo $result[0]->thainame.": ".$result[0]->artist.": ".$result[0]->key;
     }
     else{
-        echo $result[2]->name.": ".$result[2]->artist.": ".$result[2]->key;
+        echo $result[0]->name.": ".$result[0]->artist.": ".$result[0]->key;
     } ?>
         </title>
         <link rel="icon" href="../images/favicons/android-chrome-192x192.png" type="image/png" sizes="192x192">
@@ -88,7 +88,7 @@
                     <div class="row align-self-start mt-4">
                         <!-- รูปภาพ -->
                         <div class="col-md-5 col-12" id="img1">
-                            <img src="../images/cover/new-arrivals-thai-fiction/<?php echo $result[2]->img; ?>">
+                            <img src="../images/cover/new-arrivals-thai-fiction/<?php echo $result[0]->img; ?>">
                         </div>
                         <!-- book detail -->
                         <div class="col-md-7 col-12" style="float:left;">
@@ -97,24 +97,24 @@
                                     <div class="row" id="name">
                                         <div class="col-12">
                                             <?php 
-                                                if($result[2]->thainame != null){
-                                                    echo "<h1 class='title-book'>".$result[2]->thainame."</h1>";
+                                                if($result[0]->thainame != null){
+                                                    echo "<h1 class='title-book'>".$result[0]->thainame."</h1>";
                                                 }
                                             ?>
                                             <div class="row no-gutters">
                                                 <div class="col-12 ">
                                                     <?php
-                                                        if($result[2]->name != null){
-                                                        echo '<h4><b>'.$result[2]->name."</b></h4>";
+                                                        if($result[0]->name != null){
+                                                        echo '<h4><b>'.$result[0]->name."</b></h4>";
                                                         }
-                                                        echo'<h4 class="subtitle-book"><a href="">'.$result[2]->artist.'</a></h4>';
+                                                        echo'<h4 class="subtitle-book"><a href="">'.$result[0]->artist.'</a></h4>';
 
-                                                        if ($result[2]->tag == null){
+                                                        if ($result[0]->tag == null){
                                                             ;
                                                         }
                                                         else{
                                                         echo '
-                                                        <h4><b>ซีรี่ส์: <span style="color:#E54343">'.$result[2]->tag[2].'</span></b></h4>';
+                                                        <h4><b>ซีรี่ส์: <span style="color:#E54343">'.$result[0]->tag[0].'</span></b></h4>';
                                                         } ?>
                                                 </div>
                                             </div>
@@ -122,7 +122,7 @@
                                     </div>
 
                                     <div class="row" id="img2">
-                                        <img class="pt-5 pb-2" src="../images/cover/non-fiction-bestsellers/<?php echo $result[2]->img; ?>">
+                                        <img class="pt-5 pb-2" src="../images/cover/non-fiction-bestsellers/<?php echo $result[0]->img; ?>">
                                     </div>
 
                                     <div class="row" id="box">
@@ -130,9 +130,9 @@
                                             <div class="box p-4" style="margin-top:5vh">
                                             <div class="row">
                                                 <div class="col-md-6">
-                                                <span class="price">'.number_format((($result[2]->cost)-(($result[2]->cost)*(($result[2]->discount)/100))), 2, '.','').'</span><span class="baht"> บาท</span>';
-                                        echo    '<p class="card-text text-555 mb-0">ราคาปก <span><s>'.($result[2]->cost).' บาท</s></span></p>';
-                                        echo     '<p class="card-text text-555 mb-3">ลด '.number_format((($result[2]->cost)*(($result[2]->discount)/100)), 2, '.','')." บาท (".($result[2]->discount).'%)</p>';
+                                                <span class="price">'.number_format((($result[0]->cost)-(($result[0]->cost)*(($result[0]->discount)/100))), 2, '.','').'</span><span class="baht"> บาท</span>';
+                                        echo    '<p class="card-text text-555 mb-0">ราคาปก <span><s>'.($result[0]->cost).' บาท</s></span></p>';
+                                        echo     '<p class="card-text text-555 mb-3">ลด '.number_format((($result[0]->cost)*(($result[0]->discount)/100)), 2, '.','')." บาท (".($result[0]->discount).'%)</p>';
                                         echo    '<li>มีสินค้าพร้อมส่ง</li></div>';
                                         echo     '<div class="col-md-6">
                                                 <select name="number">
@@ -158,21 +158,21 @@
                                             <div class="col-12 pt-5">
                                             <?php
                                                 //  review
-                                                if ($result[2]->review == null){
+                                                if ($result[0]->review == null){
                                                     ;
                                                 }
                                                 else{
-                                                    for ( $i=0; $i<sizeof($result[2]->review); $i++){
-                                                        echo '<p class="review">'.$result[2]->review[$i].'</p>';
+                                                    for ( $i=0; $i<sizeof($result[0]->review); $i++){
+                                                        echo '<p class="review">'.$result[0]->review[$i].'</p>';
                                                         }
                                                 }
                                         
                                                 //reviewer
-                                                if ($result[2]->reviewer == null){
+                                                if ($result[0]->reviewer == null){
                                                     ;
                                                 }
                                                 else{
-                                                    echo '<p class="review"><b>- '.$result[2]->reviewer.'</b></p>';
+                                                    echo '<p class="review"><b>- '.$result[0]->reviewer.'</b></p>';
                                                 }  
                                             ?>
                                             </div>
@@ -198,44 +198,44 @@
                         <?php    
                             echo "<p class=\"thainame\">ข้อมูลหนังสือ</p>";
                             echo "<div class=\"detail\">";
-                            if($result[2]->name != null){
-                                if($result[2]->thainame != null){
-                                    echo "<b>".$result[2]->thainame."</b><br>";     
-                                    echo "แปลจากหนังสือ: <b>".$result[2]->name."</b><br>";
+                            if($result[0]->name != null){
+                                if($result[0]->thainame != null){
+                                    echo "<b>".$result[0]->thainame."</b><br>";     
+                                    echo "แปลจากหนังสือ: <b>".$result[0]->name."</b><br>";
                                 }
                                 else{
-                                    echo "<b>".$result[2]->name."</b><br>";
+                                    echo "<b>".$result[0]->name."</b><br>";
                                 }    
                             }
                             else{
-                                echo "<b>".$result[2]->thainame."</b><br>";
+                                echo "<b>".$result[0]->thainame."</b><br>";
                             }
                                 
                             echo"ผู้เขียน: ";
                             echo "<a href=\"\"><b>";
-                            echo $result[2]->artist;
+                            echo $result[0]->artist;
                             echo "</b></a><br>";
-                            if($result[2]->translater != null){
+                            if($result[0]->translater != null){
                                 echo "ผู้แปล: ";
                                 echo "<a href=\"\"><b>";
-                                echo $result[2]->translater;
+                                echo $result[0]->translater;
                                 echo "</b></a><br>";
                             }
-                            if($result[2]->coverdesigner != null){
+                            if($result[0]->coverdesigner != null){
                                 echo "ออกแบบปก: ";
                                 echo "<a href=\"\"><b>";
-                                echo $result[2]->coverdesigner;
+                                echo $result[0]->coverdesigner;
                                 echo "</b></a><br>";
                             }
-                            if($result[2]->publicher != null){
+                            if($result[0]->publicher != null){
                                 echo "สำนักพิมพ์: ";
                                 echo "<a href=\"\"><b>";
-                                echo $result[2]->publicher;
+                                echo $result[0]->publicher;
                                 echo "</b></a><br>";
                             }
-                            echo "จำนวนหน้า: ".$result[2]->pages." หน้า ".$result[2]->bookcover."<br>";
-                            echo "พิมพ์ครั้งที่ ".$result[2]->repub." — ".$result[2]->date."<br>";
-                            echo "ISBN: ".$result[2]->key;
+                            echo "จำนวนหน้า: ".$result[0]->pages." หน้า ".$result[0]->bookcover."<br>";
+                            echo "พิมพ์ครั้งที่ ".$result[0]->repub." — ".$result[0]->date."<br>";
+                            echo "ISBN: ".$result[0]->key;
                             echo "</div><br>";
                         ?>
                     </div>
@@ -244,11 +244,11 @@
                         <div id="detail">
                             <?php
 
-                            for($i=0;$i<sizeof($result[2]->description);$i++){
-                                    echo $result[2]->description[$i]."<br><br>";
+                            for($i=0;$i<sizeof($result[0]->description);$i++){
+                                    echo $result[0]->description[$i]."<br><br>";
                             }
-                            if($result[2]->credit != null){
-                            echo "<b>-".$result[2]->credit."</b>";
+                            if($result[0]->credit != null){
+                            echo "<b>-".$result[0]->credit."</b>";
                             }
                             echo "<br><br>";
                             ?>
